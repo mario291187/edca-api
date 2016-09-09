74716845a06c53708d99c65b51ff206ed0b5ce9d
Mario Torres <mario291187@gmail.com>
2016-09-09 11:04:40 -0500

    api

--- routes/api.js
+++ routes/api.js
@@ -45,73 +45,50 @@ router.post('/list/contractingprocess', function(req, res){
 
 });
 
+//planning
+router.post('/list/planning', function (req, res) {
 
-//documents
-
-//tenderers
-
-//suppliers
-
-
-/* * * * * * * * * * * * * * * *
- * Creates a new contracting   *
- * process and returns the id  *
- * * * * * * * * * * * * * * * */
-router.post('/new/contractingprocess', function(req, res){
-
-
-    edca_db.tx(function (t) {
-
-        return t.one("insert into ContractingProcess (fecha_creacion, hora_creacion, ocid, stage ) values " +
-            "(current_date, current_time, concat('NUEVA_CONTRATACION_', current_date,'_', current_time), 0) returning id").then(function (process) {
-
-            var planning = t.one("insert into Planning (ContractingProcess_id) values ($1) returning id", process.id);
-            var tender = t.one ("insert into Tender (ContractingProcess_id,status) values ($1, $2) returning id as tender_id", [process.id, 'none']);
-            var contract = t.one ("insert into Contract (ContractingProcess_id, status) values ($1, $2) returning id", [process.id, 'none']);
-
-            return t.batch([process = { id : process.id}, planning, tender, contract] );
-
-
-        }).then(function (info) {
-
-            var process= {process_id : info[0].id};
-            var planning = {planning_id : info[1].id};
-
-            return t.batch([
-                process, planning,
-                t.one("insert into Budget (ContractingProcess_id, Planning_id) values ($1, $2 ) returning id as budget_id", [info[0].id, info[1].id]),
-                t.one("insert into Buyer (ContractingProcess_id) values ($1) returning id as buyer_id",[info[0].id]),
-                t.one("insert into ProcuringEntity (contractingprocess_id, tender_id) values ($1, $2) returning id as procuringentity_id",[info[0].id, info[2].id]),
-                t.one("insert into Award (ContractingProcess_id,status) values ($1, $2) returning id as award_id", [info[0].id, 'none']),
-                t.one("insert into Implementation (ContractingProcess_id, Contract_id ) values ($1, $2) returning id as implementation_id", [info[0].id, info[3].id]),
-                t.one("insert into Publisher (ContractingProcess_id) values ($1) returning id as publisher_id", info[0].id)
-            ]);
-
-        });
-    }).then(function (data) {
-        console.log(data);
-        res.json ({
-            status : 'ok',
-            description : "Se ha creado un nuevo registro de proceso de contratación",
-            data: data
-        });
-
-    }).catch(function (error) {
-        console.log(error);
-
-        res.json({
-            status: "Error",
-            description: "",
-            data: error
-        });
-    });
 });
 
-/* * * * * * * * * * * * * * * * * *
- * Updates:                        *
- * entities created with the       *
- * /new/contractingprocess request *
- * * * * * * * * * * * * * * * * * */
+//tender
+router.post('/list/tender', function (req, res ) {
+
+});
+
+//award
+router.post('/list/award', function (req, res ) {
+
+});
+
+//contract
+router.post('/list/contract', function (req, res) {
+
+});
+
+
+//documents
+router.post('/list/documents', function(req, res){
+
+});
+
+//tenderers, suppliers, buyer
+router.post('/list/organization/:type', function (req, res) {
+
+});
+
+//items
+router.post('/list/items', function (req, res) {
+
+});
+
+//transactions
+router.post('/list/transactions', function (req, res) {
+
+});
+
+/* * * * * *
+ * Updates *
+ * * * * * */
 
 router.post('/update/contractingprocess', function (req, res){
 
@@ -395,6 +372,57 @@ router.post('/update/publisher', function (req, res){
 /* * * * * * * *
  * Insertions  *
  * * * * * * * */
+
+// new contracting process
+router.post('/new/contractingprocess', function(req, res){
+
+
+    edca_db.tx(function (t) {
+
+        return t.one("insert into ContractingProcess (fecha_creacion, hora_creacion, ocid, stage ) values " +
+            "(current_date, current_time, concat('NUEVA_CONTRATACION_', current_date,'_', current_time), 0) returning id").then(function (process) {
+
+            var planning = t.one("insert into Planning (ContractingProcess_id) values ($1) returning id", process.id);
+            var tender = t.one ("insert into Tender (ContractingProcess_id,status) values ($1, $2) returning id as tender_id", [process.id, 'none']);
+            var contract = t.one ("insert into Contract (ContractingProcess_id, status) values ($1, $2) returning id", [process.id, 'none']);
+
+            return t.batch([process = { id : process.id}, planning, tender, contract] );
+
+
+        }).then(function (info) {
+
+            var process= {process_id : info[0].id};
+            var planning = {planning_id : info[1].id};
+
+            return t.batch([
+                process, planning,
+                t.one("insert into Budget (ContractingProcess_id, Planning_id) values ($1, $2 ) returning id as budget_id", [info[0].id, info[1].id]),
+                t.one("insert into Buyer (ContractingProcess_id) values ($1) returning id as buyer_id",[info[0].id]),
+                t.one("insert into ProcuringEntity (contractingprocess_id, tender_id) values ($1, $2) returning id as procuringentity_id",[info[0].id, info[2].id]),
+                t.one("insert into Award (ContractingProcess_id,status) values ($1, $2) returning id as award_id", [info[0].id, 'none']),
+                t.one("insert into Implementation (ContractingProcess_id, Contract_id ) values ($1, $2) returning id as implementation_id", [info[0].id, info[3].id]),
+                t.one("insert into Publisher (ContractingProcess_id) values ($1) returning id as publisher_id", info[0].id)
+            ]);
+
+        });
+    }).then(function (data) {
+        console.log(data);
+        res.json ({
+            status : 'ok',
+            description : "Se ha creado un nuevo registro de proceso de contratación",
+            data: data
+        });
+
+    }).catch(function (error) {
+        console.log(error);
+
+        res.json({
+            status: "Error",
+            description: "",
+            data: error
+        });
+    });
+});
 
 // Items
 router.post('/new/item:stage', function (req, res){
