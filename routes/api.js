dcc7856101fbb3e2f65d2d8e9d463ffd5023d95a
Mario Torres <mario291187@gmail.com>
2016-09-13 11:57:51 -0500

    routess

--- routes/api.js
+++ routes/api.js
@@ -26,12 +26,12 @@ if ( typeof process.env.EDCA_DB != "undefined" ){
  * Getting information *
  * * * * * * * * * * * */
 
-router.post('/list/contractingprocess', function(req, res){
+router.post('/list/entities', function(req, res){
 
-    edca_db.manyOrNone("select * from contractingprocess ").then(function(data){
+    edca_db.manyOrNone("select * from $1~ ", [ req.body.entity ]).then(function(data){
         res.json({
             status: "Ok",
-            Description: "Listado de procesos de contratación",
+            Description: "Listado: " + req.body.entity,
             data: data
         })
     }).catch(function (error) {
@@ -45,46 +45,6 @@ router.post('/list/contractingprocess', function(req, res){
 
 });
 
-//planning
-router.post('/list/planning', function (req, res) {
-
-});
-
-//tender
-router.post('/list/tender', function (req, res ) {
-
-});
-
-//award
-router.post('/list/award', function (req, res ) {
-
-});
-
-//contract
-router.post('/list/contract', function (req, res) {
-
-});
-
-
-//documents
-router.post('/list/documents', function(req, res){
-
-});
-
-//tenderers, suppliers, buyer
-router.post('/list/organization/:type', function (req, res) {
-
-});
-
-//items
-router.post('/list/items', function (req, res) {
-
-});
-
-//transactions
-router.post('/list/transactions', function (req, res) {
-
-});
 
 /* * * * * *
  * Updates *
@@ -92,16 +52,16 @@ router.post('/list/transactions', function (req, res) {
 
 router.post('/update/contractingprocess', function (req, res){
 
-    var localid = +req.body.localid;
+    var contractingprocess_id = +req.body.localid;
     var stage = +req.body.stage;
     var ocid = req.body.ocid;
 
     //column set
 
 
-    if ( isNaN(localid) || isNaN(stage) || stage < 0 || stage > 4){
+    if ( isNaN( contractingprocess_id ) || isNaN(stage) || stage < 0 || stage > 4){
 
-        edca_db.oneOrNone(" update contractingprocess set stage = $1 where localid = $2 returning id ", [ stage, localid ]).then(function (data) {
+        edca_db.oneOrNone(" update contractingprocess set stage = $1 where localid = $2 returning id ", [ stage, contractingprocess_id ]).then(function (data) {
             res.send({
                 status:"Ok",
                 description: "Proceso de contratación: Etapa actualizada",
@@ -119,13 +79,11 @@ router.post('/update/contractingprocess', function (req, res){
         });
     }
 
-    edca_db.one('update contractingprocess set ocid = $1, stage = $2 where id = $3 returning id',
-        [
+    edca_db.one('update contractingprocess set ocid = $1, stage = $2 where id = $3 returning id', [
             ocid,
             stage,
-            localid
-        ]
-    ).then(function (data) {
+            contractingprocess_id
+        ]).then(function (data) {
         res.json({
             status: 'Ok',
             description: "Proceso de contratación actualizado",
@@ -146,16 +104,17 @@ router.post('/update/planning', function (req, res){
 
     edca_db.tx(function (t) {
 
-        var planning = this.one("update planning set rationale = $1 where ContractingProcess_id = $2 returning id",
-            [
+        return this.bath([
+            //planning
+            this.one("update planning set rationale = $1 where ContractingProcess_id = $2 returning id", [
                 req.body.rationale,
-                req.body.contractingprocess_id
-            ]);
+                req.body.contractingprocess_id //id del proceso de contratación
+            ]),
 
-        var budget = this.one("update budget set budget_source = $2, budget_budgetid =$3, budget_description= $4, budget_amount=$5, budget_currency=$6, budget_project=$7, budget_projectid=$8, budget_uri=$9" +
-            " where ContractingProcess_id=$1 returning id",
-            [
-                req.body.contractingprocess_id,
+            //budget
+            this.one("update budget set budget_source = $2, budget_budgetid =$3, budget_description= $4, budget_amount=$5, budget_currency=$6, budget_project=$7, budget_projectid=$8, budget_uri=$9" +
+                " where ContractingProcess_id=$1 returning id", [
+                req.body.contractingprocess_id, // id del proceso de contratación
                 req.body.budget_source,
                 req.body.budget_budgetid,
                 req.body.budget_description,
@@ -164,9 +123,8 @@ router.post('/update/planning', function (req, res){
                 req.body.budget_project,
                 req.body.budget_projectid,
                 req.body.budget_uri
-            ]);
-
-        return this.batch([planning, budget]);
+            ])
+        ]);
 
     }).then(function (data) {
         res.json({
@@ -185,16 +143,14 @@ router.post('/update/planning', function (req, res){
 });
 
 // organizations -> buyer, tenderers, suppliers
-router.post('/update/organization/:type', function (req, res){
-
+router.post('/update/organization/', function (req, res){
 
 
     edca_db.one("update $1~ set identifier_scheme= $3, identifier_id =$4, identifier_legalname=$5, identifier_uri=$6, name = $7, address_streetaddress=$8," +
         " address_locality=$9, address_region =$10, address_postalcode=$11, address_countryname=$12, contactpoint_name=$13, contactpoint_email=$14, contactpoint_telephone=$15," +
-        " contactpoint_faxnumber=$16, contactpoint_url=$17 where ContractingProcess_id = $2 returning id",
-        [
-            req.body.table,
-            req.body.localid,
+        " contactpoint_faxnumber=$16, contactpoint_url=$17 where ContractingProcess_id = $2 returning id", [
+            req.body.table, //  tabla donde se inserta el registro -> buyer, tendererer, supplier
+            req.body.contractingprocess_id, // id del proceso de contratación
             req.body.identifier_scheme,
             req.body.identifier_id,
             req.body.identifier_legalname,
@@ -210,8 +166,7 @@ router.post('/update/organization/:type', function (req, res){
             req.body.contactpoint_telephone,
             req.body.contactpoint_faxnumber,
             req.body.contactpoint_url
-        ]
-    ).then( function (data) {
+        ]).then( function (data) {
         res.json({
             status: 'Ok',
             description : "Comprador actualizado",
@@ -234,7 +189,7 @@ router.post('/update/tender', function (req, res){
         "tenderperiod_startdate=$16, tenderperiod_enddate=$17, enquiryperiod_startdate=$18, enquiryperiod_enddate=$19 ,hasenquiries=$20, eligibilitycriteria=$21, awardperiod_startdate=$22," +
         "awardperiod_enddate=$23, numberoftenderers=$24, amendment_date=$25, amendment_rationale=$26" +
         " where ContractingProcess_id = $1 returning id", [
-        req.body.contractingprocess_id,
+        req.body.contractingprocess_id, //id del proceso de contratación
         req.body.tenderid,
         req.body.title,
         req.body.description,
@@ -282,7 +237,7 @@ router.post('/update/award', function (req, res){
         "contractperiod_enddate=$10,amendment_date=$11,amendment_rationale=$12 " +
         " where ContractingProcess_id = $1 returning id",
         [
-            req.body.contractingprocess_id,
+            req.body.contractingprocess_id, // id del proceso de contratación
             req.body.awardid,
             req.body.title,
             req.body.description,
@@ -316,9 +271,9 @@ router.post('/update/contract', function (req, res){
     edca_db.one("update contract set contractid=$2, awardid=$3, title=$4, description=$5, status=$6, period_startdate=$7, period_enddate=$8, value_amount=$9, value_currency=$10," +
         " datesigned=$11, amendment_date=$12, amendment_rationale=$13 " +
         " where ContractingProcess_id = $1 returning id", [
-        req.body.contractingprocess_id,
-        req.body.contractid,
-        req.body.awardid,
+        req.body.contractingprocess_id, // id del proceso de contratación
+        req.body.contractid, // id de la etapa de contrato, puede ser cualquier cosa
+        req.body.awardid, // id de la etapa de adjudicación, puede ser cualquier cosa, pero debe hacer match con la etapa de adjudicación
         req.body.title,
         req.body.description,
         req.body.status,
@@ -346,15 +301,13 @@ router.post('/update/contract', function (req, res){
 
 // Publisher
 router.post('/update/publisher', function (req, res){
-    edca_db.one("update publisher set name=$2, scheme=$3, uid=$4, uri=$5 where id = $1 returning id",
-        [
-            req.body.id,
+    edca_db.one("update publisher set name=$2, scheme=$3, uid=$4, uri=$5 where id = $1 returning id", [
+            req.body.contractingprocess_id, //id del proceso de contratación
             req.body.name,
             req.body.scheme,
             req.body.uid,
             req.body.uri
-        ]
-    ).then(function (data) {
+        ]).then(function (data) {
         res.json({
             status:  "Ok",
             description : "Publisher actualizado",
@@ -376,18 +329,17 @@ router.post('/update/publisher', function (req, res){
 // new contracting process
 router.post('/new/contractingprocess', function(req, res){
 
-
     edca_db.tx(function (t) {
 
         return t.one("insert into ContractingProcess (fecha_creacion, hora_creacion, ocid, stage ) values " +
             "(current_date, current_time, concat('NUEVA_CONTRATACION_', current_date,'_', current_time), 0) returning id").then(function (process) {
 
-            var planning = t.one("insert into Planning (ContractingProcess_id) values ($1) returning id", process.id);
-            var tender = t.one ("insert into Tender (ContractingProcess_id,status) values ($1, $2) returning id as tender_id", [process.id, 'none']);
-            var contract = t.one ("insert into Contract (ContractingProcess_id, status) values ($1, $2) returning id", [process.id, 'none']);
-
-            return t.batch([process = { id : process.id}, planning, tender, contract] );
-
+            return t.batch([
+                process = { id : process.id},
+                t.one("insert into Planning (ContractingProcess_id) values ($1) returning id", [process.id]),
+                t.one ("insert into Tender (ContractingProcess_id,status) values ($1, $2) returning id as tender_id", [process.id, 'none']),
+                t.one ("insert into Contract (ContractingProcess_id, status) values ($1, $2) returning id", [process.id, 'none'])
+            ]);
 
         }).then(function (info) {
 
@@ -418,21 +370,19 @@ router.post('/new/contractingprocess', function(req, res){
 
         res.json({
             status: "Error",
-            description: "",
+            description: "Ha ocurrido un error al crear el nuevo proceso de contratación",
             data: error
         });
     });
 });
 
 // Items
-router.post('/new/item:stage', function (req, res){
-
+router.post('/new/item', function (req, res){
 
     edca_db.one('insert into $1~ (contractingprocess_id, itemid, description, classification_scheme, classification_id, classification_description, classification_uri,' +
-        ' quantity, unit_name, unit_value_amount, unit_value_currency) values ($2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) returning id',
-        [
-            req.body.table,
-            req.body.localid,
+        ' quantity, unit_name, unit_value_amount, unit_value_currency) values ($2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) returning id', [
+            req.body.table, // tabla donde se inserta -> awarditems, ...
+            req.body.contractingprocess_id, // id del proceso de contratación
             req.body.itemid,
             req.body.description,
             req.body.classification_scheme,
@@ -443,8 +393,7 @@ router.post('/new/item:stage', function (req, res){
             req.body.unit_name,
             (isNaN(req.body.unit_value_amount)?null:req.body.unit_value_amount),
             req.body.unit_value_currency
-        ]
-    ).then(function (data) {
+        ]).then(function (data) {
         res.json({
             status:  "Ok",
             description : "Se ha creado un nuevo artículo",
@@ -462,11 +411,11 @@ router.post('/new/item:stage', function (req, res){
 });
 
 // Amendment changes
-router.post('/new/amendmentchange:stage', function (req, res){
+router.post('/new/amendmentchange', function (req, res){
 
     edca_db.one('insert into $1~ (contractingprocess_id, property, former_value) values ($2,$3,$4) returning id',[
-        req.body.table,
-        req.body.localid,
+        req.body.table, //tabla donde se inserta el cambio
+        req.body.contractingprocess_id, // id del proceso de contratación
         req.body.property,
         req.body.former_value
     ]).then(function (data) {
@@ -486,13 +435,13 @@ router.post('/new/amendmentchange:stage', function (req, res){
 });
 
 // Organizations -> tenderers, suppliers
-router.post('/new/organization:type', function (req, res){
+router.post('/new/organization', function (req, res){
     edca_db.one("insert into $17~" +
         " (contractingprocess_id, identifier_scheme, identifier_id, identifier_legalname, identifier_uri, name, address_streetaddress," +
         " address_locality, address_region, address_postalcode, address_countryname, contactpoint_name, contactpoint_email, contactpoint_telephone," +
         " contactpoint_faxnumber, contactpoint_url) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16) returning id",
         [
-            req.body.localid,
+            req.body.contractingprocess_id, // id del proceso de contratación
             req.body.identifier_scheme,
             req.body.identifier_id,
             req.body.identifier_legalname,
@@ -508,7 +457,7 @@ router.post('/new/organization:type', function (req, res){
             req.body.contactpoint_telephone,
             req.body.contactpoint_faxnumber,
             req.body.contactpoint_url,
-            req.body.table
+            req.body.table // tabla donde se inserta la organización -> tenderer, supplier
         ]
     ).then(function (data) {
         res.send({
@@ -532,7 +481,7 @@ router.post('/new/transaction', function (req, res){
         'providerorganization_scheme,providerorganization_id,providerorganization_legalname,providerorganization_uri,' +
         'receiverorganization_scheme,receiverorganization_id,receiverorganization_legalname,receiverorganization_uri, uri) ' +
         'values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15) returning id',[
-        req.body.localid,
+        req.body.contractingprocess_id, // id del proceso de contratación
         req.body.transactionid,
         req.body.source,
         (req.body.implementation_date != '')?req.body.implementation_date:null,
@@ -571,22 +520,18 @@ router.post('/new/transaction', function (req, res){
 router.post('/new/document', function (req, res){
     edca_db.one('insert into $1~ (contractingprocess_id, document_type, documentid, title, description, url, date_published, date_modified, format, language) values ($2,$3,$4,$5,$6,$7,$8,$9,$10,$11) returning id',
         [
-            req.body.table,
-            req.body.localid,
+            req.body.table, //tabla donde se inserta el documento
+            req.body.contractingprocess_id, // id del proceso de contratación
             req.body.document_type,
-            req.body.documentid,
+            req.body.documentid, //id del ducumento, puede ser cualquier cosa
             req.body.title,
             req.body.description,
             req.body.url,
             (req.body.date_published!='')?req.body.date_published:null,
             (req.body.date_modified!='')?req.body.date_modified:null,
             req.body.format,
-            req.body.language
+            req.body.language // lenguaje del documento en código de dos letras
         ]).then(function (data) {
-        res.send("Se ha creado un nuevo documento");
-        console.log("new "+ req.body.table + ": ", data);
-
-    }).then(function (data) {
         res.json({
             status:"Ok",
             description: "Se ha registrado un nuevo documento",
@@ -606,7 +551,7 @@ router.post('/new/document', function (req, res){
  * * * * * */
 router.post('/delete/contractingprocess',function (req, res ) {
 
-    var cpid = +req.body.cpid;
+    var cpid = +req.body. contractingprocess_id; //id del proceso de contratación que se requiere eliminar
 
     if (isNaN(cpid) ){
         res.json({
